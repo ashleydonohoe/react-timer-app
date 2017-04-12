c5c99278ce74db7657c43fb3cb25dd0dfa2c2af6
Ashley Donohoe <ashleyrdonohoe@gmail.com>
2017-04-11 23:32:43 -0400

    styling clock

--- app/components/Countdown.jsx
+++ app/components/Countdown.jsx
@@ -1,10 +1,11 @@
 let React = require("react");
+let Clock = require("Clock");
 
 let Countdown = React.createClass({
     render: function () {
         return (
             <div>
-                <p>Countdown Component</p>
+                <Clock totalSeconds={129}/>
             </div>
         )
     }
